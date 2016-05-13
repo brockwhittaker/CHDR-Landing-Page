c84479f0d9186d80595c9aaeb165363a1071a168
Brock Whittaker <bjwhitta@asu.edu>
2016-05-13 02:43:16 -0600

    Working around Maitre
    Because I have to.

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,23 @@
 (function () {
   var $ = _;
-  $email = $("input[type=email]");
 
-  $("input[type=email]").click(function () {
-    $("#provide_email")
-      .removeClass("hide")
-      .css({
-        top: (this.offsetTop - 50) + "px",
-        left: (this.offsetLeft - 100 + this.offsetWidth / 2) + "px"
-      });
-  });
+  var $email;
+
+  function callback () {
+    $email.click(function () {
+      $("#provide_email")
+        .removeClass("hide")
+        .css({
+          top: (this.offsetTop - 50) + "px",
+          left: (this.offsetLeft - 100 + this.offsetWidth / 2) + "px"
+        });
+    });
+  }
+
+  var ugh = setInterval(function () {
+    $email = $("input[type=email]");
+    if ($email[0]) { clearInterval(ugh), callback(); }
+  }, 100);
 
   $(window).on("resize", function () {
     $("#provide_email")
@@ -17,6 +25,10 @@
         top: ($email[0].offsetTop - 50) + "px",
         left: ($email[0].offsetLeft - 100 + $email[0].offsetWidth / 2) + "px"
       });
+  });
+
+  $("#sign_up").click(function () {
+    smoothScroll.animateScroll(3000);
   });
 
   $("#down_arrow").click(function () {
